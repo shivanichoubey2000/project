544ab35a04e8217de2c7788c35d8f0135e866a90
shivanichoubey2000 <choubeyshivani24@gmail.com>
2025-07-22 11:54:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const Host = '0.0.0.0';
 //App
 const app = express();
 app.get('/', (req,res) => {
-res.send('Hello Devops Cloud World. Thanks!!!!!!!');
+  res.send('Hello Devops Cloud World. Thanks!!!!!!!');
 });
 
 app.listen(PORT, HOST);
